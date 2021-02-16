1ca3a55bfce5b6cf1af09a373e1ff0a5623e813c
Kor Dwarshuis <kor@dwarshuis.com>
2021-02-16 16:39:30 +0100

    Added html for accordeon

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,17 +2,19 @@
 <div class="hello">
     <!-- <h1>{{ msg }}</h1> -->
 
-    <div v-for="(item, index) in msg" :key="item.Long" class="card">
-        <div class="card-header" id="heading12">
-            <h2 class="mb-0">
-                <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" :data-target="'#collapse' + index" aria-expanded="true" :aria-controls="'collapse' + index">
-                    {{item["'Short"]}}
-                </button>
-            </h2>
-        </div>
-        <div :id="'collapse'+ index" class="collapse" aria-labelledby="heading12" data-parent="#accordionExample">
-            <div class="card-body">
-              {{item.Long}}
+    <div class="accordion" id="accordionExample">
+        <div v-for="(item, index) in msg" :key="item.Long" class="card">
+            <div class="" :id="'heading' + index">
+                <h2 class="mb-0">
+                    <button class="btn btn-link btn-block text-left" style="text-transform: none;" type="button" data-toggle="collapse" :data-target="'#collapse' + index" aria-expanded="false" :aria-controls="'collapse' + index">
+                        {{item["'Short"]}} <span class="read-more" style="position: absolute; right: 0; bottom: 0; line-height: 1; font-size: 2em;">+</span>
+                    </button>
+                </h2>
+            </div>
+            <div :id="'collapse'+ index" class="collapse p-5 text-left" aria-labelledby="'heading' + index" data-parent="#accordionExample">
+                <div class="">
+                    {{item.Long}}
+                </div>
             </div>
         </div>
     </div>
@@ -23,16 +25,16 @@
 <script>
 export default {
     name: "HelloWorld",
-    data: function() {
-      return {
-        counter: 0
-      }
+    data: function () {
+        return {
+            counter: 0
+        }
     },
     methods: {
-      incrementCounter() {
-        this.counter++;
-        return this.counter;
-      }
+        incrementCounter() {
+            this.counter++;
+            return this.counter;
+        }
     },
     props: {
         msg: Array
@@ -59,5 +61,9 @@ li {
 
 a {
     color: #42b983;
+}
+
+button[aria-expanded="true"] span {
+  display: none;
 }
 </style>
